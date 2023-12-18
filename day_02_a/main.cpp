24778c3f6a7c2737afb47fcd31c62434c3cdf273
Aleksander Dudek <a.dudek717@gmail.com>
2023-12-18 20:27:44 +0100

    Day 02 part1 + part2 completed

---
+++ day_02_a/main.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <fstream>
+#include <filesystem>
+#include <string>
+#include <vector>
+
+struct Combination {
+    int red = 0;
+    int green = 0;
+    int blue = 0;
+};
+
+struct Game {
+    int id = 0;
+    std::vector<Combination> combinations;
+};
+
+int main() {
+    std::filesystem::path filePath = "lines.txt";
+    if (!std::filesystem::exists(filePath)) {
+        std::cerr << "File \"" << filePath << "\" not found..." << std::endl;
+        return -1;
+    }
+
+    std::ifstream inputFile(filePath);
+    if (!inputFile.is_open()) {
+        std::cerr << "Failed to open the \"" << filePath << "\" file..." << std::endl;
+        return -1;
+    }
+
+    Game requirement{0, {{12, 13, 14}}};
+
+    std::string line;
+    std::vector<Game> games;
+    while (std::getline(inputFile, line)) {
+        std::string word;
+        bool wordIsNum = false;
+        int lastNum = 0;
+        Game newGame;
+        Combination newCombination;
+        
+        for(int i = 0; i < line.size(); ++i) {
+            // mark word as a colors number
+            if (isdigit(line.at(i))) { wordIsNum = true; }
+            
+            // save game id
+            if (line.at(i) == ':') {
+                newGame.id = stoi(word);
+                word.clear();
+                wordIsNum = false;
+                continue;
+            }
+
+            if (line.at(i) == ' ') {
+                if (wordIsNum) {
+                    lastNum = stoi(word);
+                    word.clear();
+                    wordIsNum = false;
+                    continue;
+                } else {
+                    word.clear();
+                    continue;
+                }
+            }
+
+            if (i == line.size() - 1) {
+                word += line.at(i);
+            }
+
+            // determine color, reset word, reset wordIsNum
+            if (line.at(i) == ',' || line.at(i) == ';' || i == line.size() - 1) {
+                if (word == "red") {
+                    newCombination.red = lastNum;
+                    lastNum = 0;
+                } else if (word == "blue") {
+                    newCombination.blue = lastNum;
+                    lastNum = 0;
+                } else if (word == "green") {
+                    newCombination.green = lastNum;
+                    lastNum = 0;
+                }
+                if (line.at(i) == ';') {
+                    newGame.combinations.push_back(newCombination);
+                    newCombination = Combination{0};
+                }
+                if (i == line.size() - 1) {
+                    newGame.combinations.push_back(newCombination);
+                    newCombination = Combination{0};
+                    games.push_back(newGame);
+                    newGame = Game{0};
+                }
+            }
+            word += line.at(i);
+        }
+
+        int winningGamesSum = 0;
+        for (const auto& g : games) {
+
+            std::cout << "Game: " << g.id << std::endl;
+            std::cout << "Combinations: " << g.combinations.size() << std::endl;
+            for (int i = 0; i < g.combinations.size(); ++i) {
+                std::cout << i + 1 << " - "
+                          << "R: " << g.combinations.at(i).red << " "
+                          << "G: " << g.combinations.at(i).green << " "
+                          << "B: " << g.combinations.at(i).blue << " "
+                          << std::endl;
+            }
+
+            bool gameWon = true;
+            for (const auto& c : g.combinations) {
+                if (requirement.combinations.at(0).red < c.red || 
+                    requirement.combinations.at(0).green < c.green ||
+                    requirement.combinations.at(0).blue < c.blue) {
+                        gameWon = false;
+                        break;
+                    }
+            }
+            if (gameWon) {
+                winningGamesSum += g.id;
+            }
+        }
+
+        std::cout << "Winning games: " << winningGamesSum << std::endl;
+    }
+    inputFile.close();
+}
